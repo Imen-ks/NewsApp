fb22bfe618ae97ee6d6d2f16f8ab86a462f40c21
Imen Ksouri <imen_ksouri@hotmail.fr>
2023-04-04 15:32:21 +0200

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Imen Ksouri on 04/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
